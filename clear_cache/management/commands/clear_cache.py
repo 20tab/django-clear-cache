96dc311674879d585b3bed8d96c065f49b0b10e1
Filippo Morelli <filippo@Filippo-Morellis-MacBook-Pro.local>
2016-10-24 16:39:01 +0200

    Added prompt

--- clear_cache/management/commands/clear_cache.py
+++ clear_cache/management/commands/clear_cache.py
@@ -11,17 +11,32 @@ class Command(BaseCommand):
         # Positional arguments
         parser.add_argument(
             'cache_alias', nargs='?', default='default', type=str)
+        parser.add_argument(
+            '--noinput', '--no-input',
+            action='store_false', dest='interactive', default=True,
+            help='Tells Django to NOT prompt the user for input of any kind.',
+        )
 
     def handle(self, *args, **kwargs):
         cache_alias = kwargs['cache_alias']
-        try:
-            assert settings.CACHES
-            cache = caches[cache_alias]
-            cache.clear()
-            self.stdout.write(
-                'Cache "{}" has been cleared!\n'.format(cache_alias))
-        except AttributeError:
-            raise CommandError('You have no cache configured!\n')
-        except InvalidCacheBackendError:
-            raise CommandError(
-                'Cache "{}" does not exist!\n'.format(cache_alias))
+        interactive = kwargs['interactive']
+        if interactive:
+            confirm = input("""You have requested to clear the cache "{}".
+This will IRREVERSIBLY DESTROY all data currently contained in that cache.
+Are you sure you want to do this?
+    Type 'yes' to continue, or 'no' to cancel: """.format(cache_alias))
+        else:
+            confirm = 'yes'
+
+        if confirm == 'yes':
+            try:
+                assert settings.CACHES
+                cache = caches[cache_alias]
+                cache.clear()
+                self.stdout.write(
+                    'Cache "{}" has been cleared!\n'.format(cache_alias))
+            except AttributeError:
+                raise CommandError('You have no cache configured!\n')
+            except InvalidCacheBackendError:
+                raise CommandError(
+                    'Cache "{}" does not exist!\n'.format(cache_alias))
